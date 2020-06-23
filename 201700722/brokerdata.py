224914a5849be529dc1add483036ee47ab44f45f
usac201700376 <3064599650401@ingenieria.usac.edu.gt>
2020-06-23 16:41:23 -0600

    modificando

--- 201700722/brokerdata.py
+++ 201700722/brokerdata.py
@@ -1,10 +1,15 @@
-#Parametros de conexion
+#HANC Parametros de conexion
 MQTT_HOST = "157.245.82.242"
 MQTT_PORT = 1883
+#HANC Estos son los nombres de archivos fijos para las credenciales del cliente MQTT
+#Comentario y clase hecho por: HANC
+USER_FILENAME ='usuario'
+SALAS_FILENAME = 'salas'
+DEFAULT_DELAY = 2
 
-#Credenciales
-#Se acostumbra solicitar al usuario que ingrese su user/pass
-#no es buena practica dejar escritas en el codigo las credenciales
+#HANC Credenciales
+#HANC Se acostumbra solicitar al usuario que ingrese su user/pass
+#HANC no es buena practica dejar escritas en el codigo las credenciales
 MQTT_USER = "proyectos"
 MQTT_PASS = "proyectos980"
 
